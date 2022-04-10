3bb3a66453e49dccb0f0ecfd62c94c215a162ed9
HandsomeRomanian <mat4school@gmail.com>
2022-04-10 00:18:30 -0400

    cerate backend

--- apps/cli/src/app/commands/initialize.command.ts
+++ apps/cli/src/app/commands/initialize.command.ts
@@ -2,13 +2,13 @@ import { ArgumentsCamelCase, CommandModule } from 'yargs';
 import { BaseCommand } from '../models/base-command';
 
 export class InitilializeCommand extends BaseCommand implements CommandModule {
-	command = 'init';
-	aliases? = ['initialize'];
-	describe = 'Initialize a new plugin';
-	builder(thing) {
+	public command = 'init';
+	public aliases = ['initialize'];
+	public describe = 'Initialize a new plugin';
+	public builder(thing) {
 		return thing;
 	}
-	handler(args: ArgumentsCamelCase) {
+	public handler(args: ArgumentsCamelCase) {
 		console.log('Initialize a new plugin', args);
 	}
 }
---
+++ libs/data-contracts/src/lib/data-contracts.spec.ts
@@ -0,0 +1,7 @@
+import { dataContracts } from './data-contracts';
+
+describe('dataContracts', () => {
+	it('should work', () => {
+		expect(dataContracts()).toEqual('data-contracts');
+	});
+});
