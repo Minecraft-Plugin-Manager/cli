25789e515275e43767a6e9f6126fcc24e07e3b76
HandsomeRomanian <mat4school@gmail.com>
2022-04-10 00:23:58 -0400

    implement swagger on api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,14 +4,26 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
 	const globalPrefix = 'api';
 	app.setGlobalPrefix(globalPrefix);
+
+	const config = new DocumentBuilder()
+		.setTitle('Minecraft Plugin Manager')
+		.setDescription('Minecraft Plugin Manager API description')
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
 	const port = process.env.PORT || 3333;
 	await app.listen(port);
 	Logger.log(
