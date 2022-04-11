7235c25e682f014ddf43066299fe9c597c18d1c8
HandsomeRomanian <mat4school@gmail.com>
2022-04-10 23:54:43 -0400

    exit app if not command passed

--- apps/cli/src/app/app.ts
+++ apps/cli/src/app/app.ts
@@ -2,5 +2,8 @@ import { container } from 'tsyringe';
 import { ServerManager } from './managers/server.manager';
 
 export function initialize(args): void {
+	if (args._.length < 1) {
+		return;
+	}
 	container.resolve(ServerManager).checkForUpdate();
 }
